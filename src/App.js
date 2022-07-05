f2071ce5e8b011e17db5fc324a87cab5d04cf979
Kevin Lai <kevinlai@kevins-air.home>
2022-07-05 21:37:25 +1000

    add other files

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import { wordle } from "./data";
+
+function App() {
+  return <div className="App">Wordle</div>;
+}
+
+export default App;
