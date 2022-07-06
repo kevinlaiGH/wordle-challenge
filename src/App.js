e64559c9f5564b4018acc744e56b84ba69f272c1
Kevin Lai <kevinlai@kevins-air.home>
2022-07-07 07:57:14 +1000

    handle game state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,38 @@ import Line from "./components/Line";
 function App() {
   const [answer, setAnswer] = useState("");
   const [guesses, setGuesses] = useState(Array(6).fill(null));
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [isGameOver, setIsGameOver] = useState(false);
+
+  useEffect(() => {
+    const handleType = (event) => {
+      if (isGameOver) {
+        return;
+      }
+      if (event.key === "Enter") {
+        if (currentGuess.length !== 5) {
+          return;
+        }
+        const isCorrect = answer === currentGuess;
+        if (isCorrect) {
+          setIsGameOver(true);
+        }
+      }
+
+      if (event.key === "Backspace") {
+        setCurrentGuess(currentGuess.slice(0, -1));
+        return;
+      }
+
+      if (currentGuess.length >= 5) {
+        return;
+      }
+
+      setCurrentGuess((oldGuess) => oldGuess + event.key);
+    };
+    window.addEventListener("keydown", handleType);
+    return () => window.removeEventListener("keydown", handleType);
+  }, [currentGuess]);
 
   useEffect(() => {
     const fetchWord = async () => {
@@ -21,9 +53,11 @@ function App() {
     <div className="board">
       <h1 className="App">Wordle</h1>
       {/* <h1>{answer}</h1> */}
-      {guesses.map((guess) => {
-        return <Line guess={guess ?? ""} />;
+      {guesses.map((guess, i) => {
+        const isCurrentGuess = i === guesses.findIndex((val) => val == null);
+        return <Line guess={isCurrentGuess ? currentGuess : guess ?? ""} />;
       })}
+      {currentGuess}
     </div>
   );
 }
